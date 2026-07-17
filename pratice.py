4182b9e771d07ce5b12ae281409dd64b0264e284
maaviakhan666i-code <maaviakhan666i@gmail.com>
2026-07-16 22:24:35 -0700

    pratice

---
+++ pratice.py
@@ -0,0 +1,18 @@
+print("maavia khan")
+name = "shakir"
+age = 20
+
+print(type(name))
+print(type(age))
+class_name = "10th"
+print(type(class_name))
+number = 9.99
+print(type(number))
+a = 500
+b = 800
+sum = a+b
+print(type(sum))
+a = None
+print(a)
+name = False
+print(type(name))
